6b83da62be14c9fdcfdd5c3a2c5c7778fd539585
EC2 Default User <ec2-user@ip-172-31-40-197.ap-northeast-1.compute.internal>
2023-10-07 09:35:02 +0000

    [Add]課題7フロントエンド

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,7 +7,16 @@ class Group < ApplicationRecord
   validates :introduction, presence: true
   has_one_attached :group_image
   
+  def get_group_image　
+    (group_image.attached?) ? group_image : 'no_image.jpg'
+  end
+  # このメソッドはgroup_imageがアタッチ（存在）しているかチェックしている。
+  # group_imageがアタッチされていれば返され、されていなければno_image.jpgと文字列が返される。
+  
   def is_owned_by?(user)
     owner.id == user.id
   end
+  # この部分は引数のuserがgroupの作成者か調べるメソッド。
+  # groupの作成者のみgroupの編集画面を表示させるときに使用。
+  
 end
